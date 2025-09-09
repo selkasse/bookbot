8e73d16311e33f13573a62dda30b2ab724e4e514
Sharif Elkassed <selkasse@gmu.edu>
2025-09-08 23:13:53 -0400

    complete report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_by_character
 
 def get_book_text(filepath):
     file_contents = None
@@ -8,11 +8,32 @@ def get_book_text(filepath):
 
     return file_contents
 
+def print_report(filepath, word_count, sorted_characters):
+    report_string = '============ BOOKBOT ============\n'
+    report_string += 'Analyzing book found at ' + filepath + '...\n'
+    report_string += '----------- Word Count ----------\n'
+    report_string += f'Found {word_count} total words\n'
+    report_string += '--------- Character Count -------\n'
+
+
+    for character_dict in sorted_characters:
+        character = character_dict["character"]
+        count = character_dict["num"]
+
+        if character.isalpha():
+            #report_string += character + ': ' + count  + '\n'
+            report_string += f'{character}: {count}\n'
+
+    print(report_string)
+
+
 
 def main():
-   #print(get_book_text('books/frankenstein.txt'))
-   frankenstein = get_book_text('books/frankenstein.txt')
-   #print(f"{get_num_words(frankenstein)} words found in the document")
-   print(count_characters(frankenstein))
+    filepath = 'books/frankenstein.txt'
+    frankenstein = get_book_text(filepath)
+    word_count = get_num_words(frankenstein)
+    character_counts = count_characters(frankenstein)
+    sorted_characters = sort_by_character(character_counts) 
 
+    print_report(filepath, word_count, sorted_characters)
 main()
--- stats.py
+++ stats.py
@@ -11,5 +11,26 @@ def count_characters(text):
             character_counts[character] += 1
         else:
             character_counts[character] = 1
-
     return character_counts
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(items):
+    return items["num"]
+
+def sort_by_character(character_counts):
+    character_dictionaries = []
+
+    for key in character_counts:
+        character_dictionary = {}
+
+        character_dictionary["character"] = key
+        character_dictionary["num"] = character_counts[key]
+
+        character_dictionaries.append(character_dictionary)
+
+    
+    character_dictionaries.sort(reverse=True, key=sort_on)
+    return character_dictionaries
+
+
